53c8352afe7f8307a6bbc2d8d481d57331099c20
BrunoConceica0 <bd687353@gmail.com>
2024-02-19 21:44:47 -0300

    adicionado páginação por slide

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ import { SlideNav } from "./slide.js";
 
 const slide = new SlideNav(".slide", ".wrapper");
 slide.init();
-console.log(slide);
 slide.changeSlide(0);
 slide.activePrevSlide();
 slide.addArrow(".prev", ".next");
-console.log(slide);
+slide.addControl();
